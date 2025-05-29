95a6495d0f38ca331caa50a6155a22015f0e80b5
kayandenizo <kayan.denizo@gmail.com>
2025-05-29 18:24:46 -0300

    Exibindo a quantidade de itens da lista

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const category = document.querySelector('#category');
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector('ul');
+const expensesQuantity = document.querySelector('aside header p span');
 
 // Captyra o evento de input para formator o valor
 amount.oninput = () => {
@@ -48,6 +49,7 @@ form.onsubmit = (e) => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista
 function expenseAdd(newExpense) {
     try {
         // Cria o elemento para adicionar o item na lista
@@ -93,9 +95,27 @@ function expenseAdd(newExpense) {
         // Adiciona o item na lista
         expenseList.append(expenseItem)
 
+        // Atualiza os totais
+        updateTotals(); 
+
 
     } catch (error) {
         alert("Nao foi possivel atualizar a lsita de despesas")
         console.log(error)
+    }
+}
+
+// Atualiza os totais
+function updateTotals() {
+    try {
+    // Recupera os itens (li) da lista (ul)
+    const items = expenseList.children
+    
+    // Atualiza a quantidade de itens da lista
+    expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+    } catch (error) {
+        console.log(error)
+        alert("Nao foi possivel atualizar os totais")
     }
 }
